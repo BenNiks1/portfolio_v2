14ae38f43aa66396515ee7c1e0a15d8db440518b
bogdan_s <bogdan_s@webant.ru>
2022-03-11 16:46:13 +0300

    Добавляет redux

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -3,7 +3,7 @@ import styles from "./Window.module.scss";
 export const Window = () => {
   return (
     <div className={styles.container}>
-      <div></div>
+      <header className={styles.header}></header>
     </div>
   );
 };
