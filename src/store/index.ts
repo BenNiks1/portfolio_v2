b7967cc6e69ffe63317fd1f2866ec44ec21cfcd9
BogdanS <ikke.trodde@gmail.com>
2023-02-01 12:40:28 +0400

    feat: Убирает redux devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,4 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import { rootReducer } from './reducer'
 import thunk from 'redux-thunk'
 
-export const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    // @ts-expect-error
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+export const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
